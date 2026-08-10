11a4419f4709ea12bdb9c993833fff1199b7a932
Matthew DeMichele <m.d.demichele@gmail.com>
2026-08-09 19:44:20 -1000

    move createFileSystemStorage related types to types.ts file

--- src/types.ts
+++ src/types.ts
@@ -147,3 +147,19 @@ export interface Persistor {
   getState(): PersistorState
   subscribe(callback: PersistorSubscribeCallback): () => any
 }
+
+export interface FilesystemStorageOptions {
+  storagePath: string
+  encoding: string
+  toFileName: (name: string) => string
+  fromFileName: (name: string) => string
+}
+
+export interface FilesystemStorage {
+  config: (customOptions: Partial<FilesystemStorageOptions>) => void
+  setItem: (key: string, value: string, callback?: (error?: Error | null) => void) => Promise<void>
+  getItem: (key: string, callback?: (error?: Error | null, result?: string | null) => void) => Promise<string | null | undefined>
+  removeItem: (key: string, callback?: (error?: Error | null) => void) => Promise<undefined>
+  getAllKeys: (callback?: (error?: Error | null, keys?: Array<string>) => any) => Promise<string[] | undefined>
+  clear: (callback?: (error?: Error | null, allKeysCleared?: boolean) => void) => Promise<boolean>
+}
